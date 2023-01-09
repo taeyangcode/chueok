1d9ec0419f015a6d042bcb904dff5a83551bee8f
taeyangcode <coreymostero@gmail.com>
2023-01-09 01:02:50 -0800

    base polaroid art constructed

---
+++ pages/404.tsx
@@ -0,0 +1,3 @@
+export default function Custom404() {
+	return <h1>404 - a memory yet to be made</h1>;
+}
---
+++ pages/home/index.tsx
@@ -0,0 +1,22 @@
+import styles from "./Home.module.scss";
+
+export default function Home() {
+	return (
+		<div className={styles.homeContainer}>
+			<div className={styles.polaroidContainer}>
+				<div className={styles.polaroidHead}>
+					<div className={styles.flashCover}></div>
+					<div className={styles.selfTimerButton}></div>
+					<div className={styles.infraredLED}></div>
+					<div className={styles.lens}></div>
+					<div className={styles.viewFinder}></div>
+					<div className={styles.lightenDarkenSwitch}></div>
+					<div className={styles.shutterSwitch}></div>
+					<div className={styles.lightMeter}></div>
+					<div className={styles.logo}></div>
+				</div>
+				<div className={styles.polaroidBody}></div>
+			</div>
+		</div>
+	);
+}
